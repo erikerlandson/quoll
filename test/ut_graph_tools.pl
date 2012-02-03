923d325f9bd3ff19f3092d945acc3531d8fce1fc
Erik Erlandson <eerlands@redhat.com>
2012-02-03 16:43:02 -0700

    some more unit tests for graph_tools

--- test/ut_graph_tools.pl
+++ test/ut_graph_tools.pl
@@ -9,10 +9,37 @@
 
 :- begin_tests(graph_tools_test).
 
-test(t1) :- vertices_edges_to_ugraph([1], [], G),
-            vertices_edges_to_ugraph([2], [], T),
-            embed(G, T, E),
-            assoc_to_list(E, L),
-            assertion(L == [1-2]).
+test('embedding an empty graph to empty graph', [nondet]) :- 
+    vertices_edges_to_ugraph([], [], G),
+    vertices_edges_to_ugraph([], [], T),
+    embed(G, T, E),
+    assoc_to_list(E, L),
+    assertion(L == []).
+
+test('embedding an empty graph to nonempty graph', [nondet]) :- 
+    vertices_edges_to_ugraph([], [], G),
+    vertices_edges_to_ugraph([a], [], T),
+    embed(G, T, E),
+    assoc_to_list(E, L),
+    assertion(L == []).
+
+test('embedding nonempty -> empty should fail', [fail]) :- 
+    vertices_edges_to_ugraph([1], [], G),
+    vertices_edges_to_ugraph([], [], T),
+    embed(G, T, _).
+
+test('embedding single node -> single node', [nondet]) :- 
+    vertices_edges_to_ugraph([1], [], G),
+    vertices_edges_to_ugraph([a], [], T),
+    embed(G, T, E),
+    assoc_to_list(E, L),
+    assertion(L == [1-a]).
+
+test('embedding a one-edge -> triangle', [nondet]) :- 
+    vertices_edges_to_ugraph([], [1-2], G),
+    vertices_edges_to_ugraph([], [a-b,b-c,c-a], T),
+    embed(G, T, E),
+    assoc_to_list(E, L),
+    assertion(L == [1-a,2-b]).
 
 :- end_tests(graph_tools_test).
